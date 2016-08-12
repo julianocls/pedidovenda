c8b09b3183e1f127f3dc5a8fddc1f0107545a2c8
Juliano <Juliano@DESKTOP-C8PEQ1R>
2016-08-11 23:39:27 -0300

    Signed-off-by: Juliano <Juliano@DESKTOP-C8PEQ1R>

--- src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,11 +1,15 @@
 package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
+import com.algaworks.pedidovenda.repository.Categorias;
 
 @Named
 @ViewScoped
@@ -13,17 +17,32 @@ public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private Categorias categorias;
+
 	private Produto produto;
-	
+
+	private List<Categoria> categoriasRaizes;
+
 	public CadastroProdutoBean() {
 		produto = new Produto();
 	}
-	
+
+	public void inicializar() {
+		System.out.println("Inicializando...");
+
+		categoriasRaizes = categorias.raizes(); 
+	}
+
 	public void salvar() {
 	}
 
 	public Produto getProduto() {
 		return produto;
+	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
 	}
 
 }
